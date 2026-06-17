22f23e4d19395d708d778d93e35bc0e057def77d
AllaP47 <allapaschinska1@gmail.com>
2026-06-17 18:40:53 +0300

    add message

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import { fetchNoteById } from '@/lib/api';
 import { Modal } from '@/components/Modal/Modal';
 
-import css from '@/app/notes/notes.module.css';
+import css from '@/app/notes/[id]/details.module.css'
 
 interface NotePreviewClientProps {
   id: string;
